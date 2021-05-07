898022153da6df92eb3d1f996efd45690b29f40d
Sam-Chorro <82852489+Sam-Chorro@users.noreply.github.com>
2021-05-07 18:57:32 -0300

    Add files via upload

---
+++ ej1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Mi primer php</H1>
+    <?php   
+
+        echo "Mi nombre es:  Samuel Chorro";
+        echo "<hr>";
+        echo "Un saludo";
+    ?>
+</body>
+</html>
